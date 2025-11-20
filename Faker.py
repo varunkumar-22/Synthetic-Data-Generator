b72fe716235c1707789e6fc9cf00020b1100d9ea
Sriram Varun Kumar <sriramvarunkumar@gmail.com>
2025-11-20 14:26:08 +0530

    Update Faker.py

--- Faker.py
+++ Faker.py
@@ -1,16 +1,15 @@
+#Impoting Faker and Pandas
 from faker import Faker
 import pandas as pd
 fake=Faker(locale="en_US")
-# print(fake.job())
-# print(fake.name())
-
+#Synthetic Employee Data Generation
 def create_employee_data(user_input):
     employee_list=[]
     for i in range(0,user_input):
         employee={}
         employee["Name"]=fake.name()
         employee["Job"]=fake.job()
-        # employee["department"]=fake.random_element("IT","Finance","Marketing","HR")
+        employee["department"]=fake.random_element("IT","Finance","Marketing","HR")
         employee["Email"]=fake.email()
         employee["Mobile_Number"]=fake.phone_number()
         employee["Salary"]=fake.random_int(min=20000,max=1600000,step=1000)
