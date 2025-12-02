10d5053056658a3e324b2cc90c954a4cd53fda95
chaithanya <chaithanyyal55@gmail.com>
2025-12-02 07:49:24 -0800

    Generating data from engine

---
+++ generater.py
@@ -0,0 +1,5 @@
+print("\nGenerating synthetic data...")
+start=time.time()
+df = generate_from_template(template, selected_subcats, count, seed)
+end=time.time()
+print(f"Time Taken: {end - start}")
