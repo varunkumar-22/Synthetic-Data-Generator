03e8497700d13a04471c50d53c652b16cc7d1807
Sriram Varun Kumar <sriramvarunkumar@gmail.com>
2025-12-02 14:25:19 +0530

    Delete save.py

--- save.py
+++
@@ -1,9 +0,0 @@
-filename = f"{template_name}_output.csv"
-df.to_csv(filename, index=False)
-
-print(f"\n✅ Data generated successfully!")
-print(f"📁 Saved as: {filename}\n")
-
-
-if __name__ == "__main__":
-    main()
