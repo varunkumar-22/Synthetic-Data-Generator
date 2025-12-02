58ba8c7af1f78d74f8478855b59eed66ec492e04
Sriram Varun Kumar <sriramvarunkumar@gmail.com>
2025-12-02 14:25:52 +0530

    Delete validation.py

--- validation.py
+++
@@ -1,8 +0,0 @@
-try:
-        template_index = int(template_choice) - 1
-        template_name = list(TEMPLATES.keys())[template_index]
-    except (ValueError, IndexError):
-        print("❌ Invalid template selection!")
-        return
-
-    template = TEMPLATES[template_name]
